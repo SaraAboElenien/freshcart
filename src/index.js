4ef67e6ad255039a2f3ff499e9e77b2120cdf8d9
SaraAboElenien <minalona383@gmail.com>
2025-01-01 17:18:00 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import UserContextProvider from './Context/UserContext';
+// import { QueryClientProvider, QueryClint } from 'react-query'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// let queryClint = new QueryClint();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    // <QueryClientProvider client={queryClint}>
+        <UserContextProvider>
+            <App />
+        </UserContextProvider>
+    // </QueryClientProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
